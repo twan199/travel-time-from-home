742431d7905cf056caeacb31f384fc2c6a4b5c41
twan199 <t.m.j.bouts@student.tue.nl>
2024-06-12 15:32:45 +0200

    Added structure

--- pathbetweenpoints.py
+++ pathbetweenpoints.py
@@ -1,7 +1,8 @@
-from routingpy import Graphhopper
 import folium
+from routingpy import Graphhopper
 
-filename = 'apikey'
+
+# Helper functions
 
 
 def get_file_contents(filename):
@@ -13,36 +14,52 @@ def get_file_contents(filename):
         print("'%s' file not found" % filename)
 
 
-api_key = get_file_contents(filename)
-# Some locations in Berlin
-coords = [[13.413706, 52.490202],
-          [13.421838, 52.514105],
-          [13.453649, 52.507987],
-          [13.401947, 52.543373]]
-
-client = Graphhopper(api_key=api_key)
-route = client.directions(locations=coords, profile='car')
-
-
 def convert_coord(coordinates):
+    """Convert coordinates from [lon, lat] to [lat, lon] format"""
     coordinates1 = [None, None]
     coordinates1[0] = coordinates[1]
     coordinates1[1] = coordinates[0]
     return coordinates1
 
 
-# pprint((route.geometry, route.duration, route.distance, route.raw))
-m = folium.Map(location=convert_coord(coords[0]), zoom_start=11)
-geometry = []
-for i in range(len(route.geometry)):
-    geometry.append(convert_coord(route.geometry[i]))
+# Settings
+filename = 'apikey'
+coords = [[13.413706, 52.490202],
+          [13.421838, 52.514105],
+          [13.453649, 52.507987],
+          [13.401947, 52.543373]]
+profile = 'car'
 
-for i in coords:
-    folium.Marker(
-        location=convert_coord(i),
-        tooltip="Click me!",
-        icon=folium.Icon(color="green"),
-    ).add_to(m)
-folium.PolyLine(geometry, tooltip="Coast").add_to(m)
 
-m.save("index.html")
+def main():
+    # Get API key
+    api_key = get_file_contents(filename)
+
+    # Initialize Graphhopper client
+    client = Graphhopper(api_key=api_key)
+
+    # Get route from Graphhopper
+    route = client.directions(locations=coords, profile=profile)
+
+    # Create map
+    start_location = convert_coord(coords[0])
+    m = folium.Map(location=start_location, zoom_start=11)
+
+    # Add markers for coordinates
+    for coord in coords:
+        folium.Marker(
+            location=convert_coord(coord),
+            tooltip="Click me!",
+            icon=folium.Icon(color="green"),
+        ).add_to(m)
+
+    # Add route polyline
+    geometry = [convert_coord(coord) for coord in route.geometry]
+    folium.PolyLine(geometry, tooltip="Route").add_to(m)
+
+    # Save map
+    m.save("index.html")
+
+
+if __name__ == "__main__":
+    main()
